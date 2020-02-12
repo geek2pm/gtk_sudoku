cda3e561accc0ce52b5e2ce46249b0407b79ce13
geek2pm <59955605+geek2pm@users.noreply.github.com>
2020-02-12 13:46:10 +0800

    Update app.c

--- app.c
+++ app.c
@@ -132,31 +132,34 @@ void check(GtkWidget *widget,gpointer data)
             printf("[%d]ok\n",i);
         }
     }
+    GtkWidget *dialog;
     if(ok)
     {
         printf("you win!");
-        GtkWidget *dialog;
         dialog = gtk_message_dialog_new(NULL,
                 GTK_DIALOG_DESTROY_WITH_PARENT,
                 GTK_MESSAGE_INFO,
                 GTK_BUTTONS_OK,
                 "\nyou win!\n");
         gtk_window_set_title(GTK_WINDOW(dialog), "success");
-        gtk_dialog_run(GTK_DIALOG(dialog));
-        gtk_widget_destroy(dialog);
+        
     }else
     {
         printf("you lose!");
-        GtkWidget *dialog;
+        
         dialog = gtk_message_dialog_new(NULL,
                 GTK_DIALOG_DESTROY_WITH_PARENT,
                 GTK_MESSAGE_INFO,
                 GTK_BUTTONS_OK,
                 "\nyou lose!\n");
         gtk_window_set_title(GTK_WINDOW(dialog), "fail");
-        gtk_dialog_run(GTK_DIALOG(dialog));
-        gtk_widget_destroy(dialog);
+        
     }
+    // set center
+    gtk_window_set_position(GTK_WINDOW(dialog), GTK_WIN_POS_CENTER_ON_PARENT);
+    gtk_window_set_transient_for(dialog,window);
+    gtk_dialog_run(GTK_DIALOG(dialog));
+    gtk_widget_destroy(dialog);
 }
 
 void change(GtkEntry *entry,gchar *preedit,gpointer data)
